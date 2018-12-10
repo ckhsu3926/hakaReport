b6689f83c6f933b2764ac98bbcc2c4b91300d6c4
許家愷 <ckhsu@ckhsu-MBPR.local>
2018-12-10 22:38:55 +0800

    migrate sqlite and init main table

--- database/migrations/2018_12_10_142721_create_main_table.php
+++ database/migrations/2018_12_10_142721_create_main_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateMainTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('main', function (Blueprint $table) {
+            $table->timestamp("upload_time");
+            $table->string("facebook_name");
+            $table->primary("upload_time");
         });
     }
 
@@ -27,6 +27,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('main');
     }
 }
